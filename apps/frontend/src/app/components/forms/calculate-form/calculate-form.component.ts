287a4afbea5748eadae34efa3e1b14405ca8f3ca
MalejaGilF <maleja12339@gamil.com>
2023-12-05 16:13:15 -0500

    creacion datos componentes

--- apps/frontend/src/app/components/forms/calculate-form/calculate-form.component.ts
+++ apps/frontend/src/app/components/forms/calculate-form/calculate-form.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'bank-damasu-calculate-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './calculate-form.component.html',
   styleUrl: './calculate-form.component.scss',
 })
-export class CalculateFormComponent {}
+export class CalculateFormComponent {
+  calculateform: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.calculateform = this.formBuilder.group({
+      amount: ['', [Validators.required]],
+      time: ['', [Validators.required]],
+      cost: [{ value: '', disabled: true }],
+    });
+  }
+
+  public onSubmit() {
+    const valorFormulario = this.calculateform.value;
+    let rentabilidad = 0;
+    console.log(valorFormulario.time);
+
+    if (Number(valorFormulario.time) === 180) {
+      rentabilidad = valorFormulario.amount * (14.7 / 100) * (180 / 365);
+    } else {
+      rentabilidad = valorFormulario.amount * (14.6 / 100) * (360 / 365);
+    }
+    console.log(rentabilidad);
+  }
+}
--- apps/frontend/src/app/components/forms/personal-info/personal-info.component.ts
+++ apps/frontend/src/app/components/forms/personal-info/personal-info.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'bank-damasu-personal-info',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './personal-info.component.html',
   styleUrl: './personal-info.component.scss',
 })
-export class PersonalInfoComponent {}
+export class PersonalInfoComponent {
+  personalInfoForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.personalInfoForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      department: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
+    });
+  }
+
+  public onSubmit() {
+    console.log(this.personalInfoForm.value);
+  }
+}
